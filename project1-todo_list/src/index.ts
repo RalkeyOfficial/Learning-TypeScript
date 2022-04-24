7646bf9a07be1681b2cecca60dd0d0dd99c87f0b
RalkeyOfficial <Ralkey@outlook.com>
2022-04-24 19:13:43 +0200

    done

--- project1-todo_list/src/index.ts
+++ project1-todo_list/src/index.ts
@@ -7,10 +7,18 @@ type Task = {
   createdAt: Date
 }
 
+// get elements
 const list = document.querySelector<HTMLUListElement>('#list');
 const form = document.getElementById('new-task-form') as HTMLFormElement | null;
+// this is just another way of doing it (getElementById doesnt support the <> syntax)
 const input = document.querySelector<HTMLInputElement>('#new-task-title');
 
+// load tasks from local storage
+const tasks: Task[] = loadTasks();
+tasks.forEach(addListItem);
+
+// on submit add task to tasks array
+// and call function to add task on the page
 form?.addEventListener("submit", e => {
   e.preventDefault()
 
@@ -22,11 +30,17 @@ form?.addEventListener("submit", e => {
     completed: false,
     createdAt: new Date()
   }
+
+  // add task to tasks array
+  tasks.push(newTask)
   
+  // add task to page
   addListItem(newTask)
   input.value = "";
 })
 
+// create and display html element for task
+// on task change 
 function addListItem(task: Task) {
   const item = document.createElement('li');
   const label = document.createElement('label');
@@ -34,7 +48,30 @@ function addListItem(task: Task) {
   checkbox.type = 'checkbox';
   checkbox.checked = task.completed;
 
+  // this SHOULD go before the previous line of code.
+  // this SHOULD not work as intended.
+  // but it does.
+  // so im not changing it.
+  checkbox.addEventListener('change', () => {
+    task.completed = checkbox.checked;
+    saveTasks();
+  })
+
   label.append(checkbox, task.title);
   item.append(label);
   list?.append(item);
+
+  saveTasks();
+}
+
+// save the tasks array to local storage
+function saveTasks() {
+  localStorage.setItem('tasks', JSON.stringify(tasks));
+}
+
+// get the tasks from local storage and return it
+function loadTasks(): Task[] {
+  const tasksJSON = localStorage.getItem('tasks');
+  if (tasksJSON == null) return []
+  return JSON.parse(tasksJSON)
 }
